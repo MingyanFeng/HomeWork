51180a80735e7b297a25d33640b6f823b60e84d8
MingyanFeng <mf3026@columbia.edu>
2017-04-17 23:33:31 -0700

    Rotate Array Modified

--- Self_Practice_Array/Array_189_Rotate Array.java
+++ Self_Practice_Array/Array_189_Rotate Array.java
@@ -1,10 +1,28 @@
+/*
+Rotate an array of n elements to the right by k steps.
+
+For example, with n = 7 and k = 3, the array [1,2,3,4,5,6,7] is rotated to [5,6,7,1,2,3,4].
+
+Note:
+Try to come up as many solutions as you can, there are at least 3 different ways to solve this problem.
+
+[show hint]
+Hint:
+Could you do it in-place with O(1) extra space?
+
+Related problem: Reverse Words in a String II
+*/
+
 public class Solution {
+// Version 1: 3 Step Reverse
     public void rotate(int[] nums, int k) {
         // corner case
         if (nums == null || nums.length == 0) {
             return;
         }
         
+// 3步翻转法的基本思路我懂，但是下面这句 k %= nums.length; 我不太明白这句话的作用
+
         // 3 step reverse
         k %= nums.length;
         // step 1: reverse all
@@ -24,8 +42,8 @@ public class Solution {
             end--;
         }
     }
+    /*
+    Time Complexity: O(n); n == nums.length;
+    Space Complexity: O(1);
+    */
 }
-/*
-Time Complexity: O(n); n == nums.length;
-Space Complexity: O(1);
-*/
