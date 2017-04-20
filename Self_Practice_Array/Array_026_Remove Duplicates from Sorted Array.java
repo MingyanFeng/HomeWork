c3134bbef3209d2e35cdaba96a2bf8d188a3202f
MingyanFeng <mf3026@columbia.edu>
2017-04-19 21:59:50 -0700

    New Self Practice Added

--- Self_Practice_Array/Array_026_Remove Duplicates from Sorted Array.java
+++ Self_Practice_Array/Array_026_Remove Duplicates from Sorted Array.java
@@ -50,7 +50,7 @@ public class Solution {
     */
 
 /*
-出现Runtime Error的原因是什么？
+出现Runtime Error的原因是什么？ 解答请翻到最底下
 */
 
 
@@ -67,7 +67,7 @@ Your answer
 Expected answer
 []
 
-这个Expectd answer是一个int变量吗？
+这个Expectd answer是一个int变量吗？ 解答请翻到最底下
 
 */
 	public int removeDuplicates(int[] nums) {
@@ -85,6 +85,7 @@ if (nums.length == 1) {
     return 1;
 }
 也是可以通过测试的，但是我不明白nums.length对于一个空集输入来讲，是怎么得到一个int返回类型的，为什么不可以返回-1呢？
+解答请翻到最底下
 */
         
         /*
@@ -114,3 +115,21 @@ if (nums.length == 1) {
     Space Complexity: O(1), fast == extra O(1) space;
     */
 }
+
+
+
+/*
+    解答：为什么会出现runtime error
+    程序本身并没有错，corner cases返回值本身是合法的
+    但是，corner cases返回值在遇到LeetCode自己给的测试程序时遇到了问题
+    因为LeetCode的测试程序是会打印出从0开始到（你返回的值）个数的
+    然后你的程序在corner cases的情况下返回了-1
+    而第0个开始-1个数是非法的（个数不存在负数），所以测试程序会报错
+    正确的返回值应该是0
+    这样的话LeetCode测试会打印出第0个开始0个数，也就是没有数，也就是空
+    这样的返回值对测试程序来说是合法的
+
+    通过这个问题，可以总结出两个经验：
+    1.一定要明确各种corner case需要返回的值，如果不明确，问面试官
+    2.不要自己乱造返回值，不要随便返回-1值，除非题目明确要求，或者返回-1那步根本走不到
+*/
